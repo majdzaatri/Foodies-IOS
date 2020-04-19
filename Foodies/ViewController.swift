cf3900b746065af4565f64a36c37e6d1caabcc6a
majdzaatri <majdz-1@live.com>
2020-04-19 06:48:45 +0300

    Initial Commit

---
+++ Foodies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodies
+//
+//  Created by Majd Zaatri on 19/04/2020.
+//  Copyright © 2020 Majd Zaatri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
